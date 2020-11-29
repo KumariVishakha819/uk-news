4cb4964e302b05a904bb94d41da0bff5f5e4f2d7
Kumari Vishakha <kumari.vishakha@siemens-healthineers.com>
2020-11-29 16:12:58 +0530

    auth token in json

--- uk-backend/App.js
+++ uk-backend/App.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 app.use(cors())
 const data= require('./Category.json');
 app.get('/api/categories/:authToken', (req, res) => {
-    if(req.params.authToken == '6929e4f32c84416aafb7717803dafd48')
+    if(req.params.authToken == data.authToken)
     res.status(200).send(data.categories);
     else{
         res.status(404).send("Please provide valid app token")
