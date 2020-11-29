89fa0810109bf9ee888587324afc82e0bda094b5
Kumari Vishakha <kumari.vishakha@siemens-healthineers.com>
2020-11-29 12:26:30 +0530

    changes

---
+++ src/components/NoNewComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoNewComponent() {
+    return (
+        <div>
+            Please reload the page or wait for sometime
+        </div>
+    )
+}
